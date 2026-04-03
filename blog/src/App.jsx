7f303afb13bb9610792042366f09246d63f1a684
Deepak <dk020402007@gmail.com>
2026-04-03 13:23:53 +0530

    initialization
    Create a react Vite project on Java-script and installed tailwindCSS

---
+++ blog/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='flex items-center justify-center h-screen w-screen'>
+      <h1 className='content-center text-center h-10 w-40 bg-green-600'>Blog-Website</h1>
+    </div>
+
+    </>
+  )
+}
+
+export default App
